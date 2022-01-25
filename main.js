144d8203ec59ae65143b9e5264d112eb072958b3
utpal641 <utpalsarker250@gmail.com>
2022-01-25 20:28:05 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+
+/* 1 NO Problem */
+let number1 = 123;
+let number2 = 7;
+let reminder = number1 % number2;
+//console.log(reminder);
+const result = reminder < 10;
+console.log('This result is',result);
+
+
+/* 2 NO Problem */
+let number3 = 4;
+let number4 = 2;
+let reminder1 = number3 % number4;
+const result1 = reminder1 == 0;
+console.log('This result is',result1);
+
+
+/* 3 NO Problem */
+let number5 = 7;
+let number6 = 2;
+let reminder2 = number5 % number6;
+const result2 = reminder2 != 0;
+console.log('This result is',result2);
+
+
+/* 4 NO Problem */
+let number7 = 10;
+let number8 = 2;
+let reminder3 = number7 % number8;
+const result3 = reminder3 != 1;
+console.log('This result is',result3);
+
+
+/* 5 NO Problem */
+let number9 = 15;
+let number10 = 2;
+let reminder4 = number9 % number10;
+const result4 = reminder4 == 1;
+console.log('This result is',result4);
+
+
+/* 6 NO Problem */
+let myAge = 21;
+let result5 = myAge >= 18;
+console.log('This result is',result5); 
+
+
+/* 7 NO Problem */
+ let mobile = false;
+ let treat = false;
+ let myheadPhone = true;
+ const result6 = mobile && treat || myheadPhone;
+ console.log('This result is',result6);
+
+
+/* 8 NO Problem */
+var country = "Bangladesh";
+var index = country[2];
+var result7 = index == "g"
+console.log('This result is',result7);
+
+
+/* 9 NO Problem */
+var country = "Bangladesh"
+var index1 = country[7];
+console.log('This result is',index1);
+
+
+/* 10 NO Problem */
+var country = "Bangladesh"
+var totalLength = country.length;
+var result8 = totalLength > 10;
+console.log('This result is',result8);
+
